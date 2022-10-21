3225f238561657373aede13f95f96519dc57fe79
Filip_Mac <filiptankiewicz04@gmail.com>
2022-10-21 15:51:39 -0700

    other files removed

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 // https://flexbox.buildwithreact.com
 
 export default function App() {
   return (
     <View>
-      <SafeAreaView style={{backgroundColor: 'red'}}></SafeAreaView>
+      <SafeAreaView style={{ backgroundColor: "red" }}></SafeAreaView>
       <View
         style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          alignContent: 'center',
-        }}>
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          alignContent: "center",
+        }}
+      >
         <Text>siema</Text>
       </View>
     </View>
