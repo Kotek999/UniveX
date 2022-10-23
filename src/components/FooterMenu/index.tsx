e0b0ba8eac6b085814030903f7bb8d1a36f365e8
Filip <filiptankiewicz04@gmail.com>
2022-10-23 02:16:48 +0200

    moon added

--- src/components/FooterMenu/index.tsx
+++ src/components/FooterMenu/index.tsx
@@ -1,45 +1,95 @@
 import * as React from "react";
+import { ImageBackground, View, Image } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { BottomNavigation, Text } from "react-native-paper";
 
-const MusicRoute = () => <Text>Music</Text>;
+const backgroundImage = require("../../images/backgroundTheme.jpg");
+const moon = require("../../images/planets/moon.gif");
 
-const AlbumsRoute = () => <Text>Albums</Text>;
+const MusicRoute = () => (
+  <View>
+    <Test />
+  </View>
+);
+// const MusicRoute = () => <ImageBackground source={backgroundImage} resizeMode="cover" style={{flex: 1, justifyContent: "center"}}><Text>Content 1</Text></ImageBackground>;
 
-const RecentsRoute = () => <Text>Recents</Text>;
+const RecentsRoute = () => <Text>Content 2</Text>;
+// const RecentsRoute = () => <ImageBackground source={backgroundImage} resizeMode="cover" style={{flex: 1, justifyContent: "center"}}><Text>Content 2</Text></ImageBackground>;
 
-const NotificationsRoute = () => <Text>Notifications</Text>;
+const NotificationsRoute = () => <Text>Content 3</Text>;
+// const NotificationsRoute = () => <ImageBackground source={backgroundImage} resizeMode="cover" style={{flex: 1, justifyContent: "center"}}><Text>Content 3</Text></ImageBackground>;
+
+export const Test = () => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      alignContent: "center",
+      backgroundColor: "gray",
+    }}
+  >
+    <ImageBackground
+      resizeMode="cover"
+      source={moon}
+      style={{
+        width: 90,
+        height: 90,
+        backgroundColor: "white",
+        borderRadius: 50,
+      }}
+    ></ImageBackground>
+  </View>
+);
+
+interface NavIcons {
+  key: string;
+  title: string | any;
+  focusedIcon?: string;
+  unfocusedIcon?: string;
+}
+
+const NAV_ICONS: NavIcons[] = [
+  {
+    key: "iconFirst",
+    title: "Icon 1",
+    focusedIcon: "heart",
+    unfocusedIcon: "heart-outline",
+  },
+  {
+    key: "iconSecond",
+    title: "Icon 2",
+    focusedIcon: "heart",
+    unfocusedIcon: "heart-outline",
+  },
+  {
+    key: "iconThird",
+    title: "Icon 3",
+    focusedIcon: "bell",
+    unfocusedIcon: "bell-outline",
+  },
+];
 
 const FooterMenu = () => {
   const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    {
-      key: "music",
-      title: "Favorites",
-      focusedIcon: "heart",
-      unfocusedIcon: "heart-outline",
-    },
-    { key: "albums", title: "Albums", focusedIcon: "album" },
-    { key: "recents", title: "Recents", focusedIcon: "history" },
-    {
-      key: "notifications",
-      title: "Notifications",
-      focusedIcon: "bell",
-      unfocusedIcon: "bell-outline",
-    },
-  ]);
+  const [routes] = React.useState(NAV_ICONS);
 
   const renderScene = BottomNavigation.SceneMap({
-    music: MusicRoute,
-    albums: AlbumsRoute,
-    recents: RecentsRoute,
-    notifications: NotificationsRoute,
+    iconFirst: MusicRoute,
+    iconSecond: RecentsRoute,
+    iconThird: NotificationsRoute,
   });
+
+  const insets = useSafeAreaInsets();
 
   return (
     <BottomNavigation
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
+      safeAreaInsets={{ bottom: insets.bottom }}
+      barStyle={{ backgroundColor: "transparent" }}
+      style={{ flex: 1 }}
     />
   );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,25 +1,34 @@
 import React, { Fragment, FC } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import FooterMenu from "../FooterMenu";
-
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import isIOS from "../../rules/resolutions/isIOS";
+import { Test } from "../FooterMenu";
 // https://flexbox.buildwithreact.com
 
 const Home: FC = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Fragment>
-      <SafeAreaView style={{ backgroundColor: "red" }} />
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          alignContent: "center",
-        }}
-      >
-        <Text>Footer Menu</Text>
-      </View>
-      <FooterMenu />
-    </Fragment>
+    <View style={{ flex: 1 }}>
+      <Fragment>
+        <View style={{ paddingTop: !isIOS() ? insets.top : 0 }}>
+          {isIOS() && <SafeAreaView style={{ backgroundColor: "red" }} />}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              alignContent: "center",
+              backgroundColor: "cyan",
+            }}
+          >
+            <Text>Title</Text>
+          </View>
+        </View>
+        <FooterMenu />
+      </Fragment>
+    </View>
   );
 };
 
